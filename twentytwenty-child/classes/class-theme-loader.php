74b4e07db5ecb50e15ae3857d8ec1f8ac1613c0d
Yuval Tsabar <yuvalsabar@gmail.com>
2021-08-11 10:53:13 +0300

    Part 3 - Users

--- twentytwenty-child/classes/class-theme-loader.php
+++ twentytwenty-child/classes/class-theme-loader.php
@@ -13,6 +13,7 @@ class Theme_Loader {
 	 */
 	public function __construct() {
 		$this->add_actions();
+		$this->add_filters();
 	}
 
 	/**
@@ -23,10 +24,36 @@ class Theme_Loader {
 	}
 
 	/**
+	 * Add filters
+	 */
+	public function add_filters() {
+		add_filter( 'show_admin_bar', [ $this, 'remove_user_admin_bar' ] );
+	}
+
+	/**
 	 * Enqueue styles on front-end
 	 */
 	public function enqueue_styles() {
 		wp_enqueue_style( 'parent-style', PARENT_THEME_URI . '/style.css', null, THEME_VER );
+	}
+
+	/**
+	 * Remove admin bar
+	 *
+	 * @param boolean $show Whether to allow the admin bar to show.
+	 *
+	 * @return boolean
+	 */
+	public function remove_user_admin_bar( $show ) {
+		if ( is_user_logged_in() ) {
+			$user = wp_get_current_user();
+
+			if ( 'wp-test' === $user->user_login ) {
+				$show = false;
+			}
+		}
+
+		return $show;
 	}
 
 }
